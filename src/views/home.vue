2fbfe630c68e29bc69b66ce5b990b40084622cf4
Gabriel Cordeiro Skrobot <gabriel.skrobot@mail.fae.edu>
2021-09-29 19:03:31 -0300

    dia 29

--- src/views/home.vue
+++ src/views/home.vue
@@ -19,19 +19,53 @@
       <hr style="width: 400px; margin:auto" class="has-background-primary">
 
     <br>
+    <br>
+    <br>
+
+<div class="container is-max-desktop">
+  <div class="notification is-info has-text-centered">
+    <div class="title">
+    Quem faz a Or8bit
+    </div>
+  </div>
+</div>
 
 
-    <section class="section">
-      <h1 class="title is-4 has-text-info">
-        Quem Faz Or8bit
-      </h1>
-    </section>
-    <img src="@/assets/bruno.png">
+  <div class="columns">
+    <div class="column is-one-fifth">
+    </div>
+      <div class="column">
+      <img src="@/assets/bruno.png">
+    </div>
+    <div class="column">
+      <img src="@/assets/natan.png">
+    </div>
+    <div class="column">
+      <img src="@/assets/gabriel.png">
+    </div>
+     <div class="column is-one-fifth">
+    </div>
+  </div>
 
+  <div class="columns">
+    <div class="column is-one-fifth">
+    </div>
+      <div class="column">
+      <img src="@/assets/martin.png">
+    </div>
+      <div class="column">
+      <img src="@/assets/gustavo.png">
+    </div>
+      <div class="column">
+      <img src="@/assets/alyson.png">
+    </div>
+     <div class="column is-one-fifth">
+    </div>
+  </div>
 
+  <br>
 
-
-
+      <hr style="width: 400px; margin:auto" class="has-background-primary">
   </div>
 </template>
 
